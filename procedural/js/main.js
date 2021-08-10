4615e60eb53f02188da23fb7dc8e6288698b47e3
Blipppy <>
2021-08-09 20:45:53 -0400

    added things

---
+++ procedural/js/main.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background('BLACK');
+
+  
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
